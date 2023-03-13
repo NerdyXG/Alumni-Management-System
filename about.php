75bcc280d2f23476cc3b5be39401ea3cda33a9a4
NerdyXG <108344292+NerdyXG@users.noreply.github.com>
2023-03-13 18:38:24 +0100

    Add files via upload

---
+++ about.php
@@ -0,0 +1,24 @@
+ <!-- Masthead-->
+        <header class="masthead">
+            <div class="container h-100">
+                <div class="row h-100 align-items-center justify-content-center text-center">
+                    <div class="col-lg-10 align-self-end mb-4" style="background: #0000002e;">
+                    	 <h1 class="text-uppercase text-white font-weight-bold">About Us</h1>
+                        <hr class="divider my-4" />
+                    </div>
+                    
+                </div>
+            </div>
+        </header>
+
+    <section class="page-section">
+        <div class="container">
+        <p>
+            At the University of Ibadan, Alumni Organisation, our Jesuit mission shapes our vision.
+            <br/>Institutions should not lose contact with the alumni especially because they are a truly valuable asset. These days schools and colleges are finding it really difficult to create a bond between the campus and the alumni. Lose the data, and you have lost all control, track, and trace. Our Alumni Management System is a targeted and dedicated platform that allows control, maintenance, and analysis of our students data. Keep an open, smooth and good communication channel with the alumni so that they can represent the institution as future brand ambassadors.
+            <br/>However, our alumni and friends -- our one-of-a-kind community, you make that vision a reality, and you can help us forge a bold new chapter in our extrordinary story. Learn how you can support the largest and most comprehensive fundraising technical support, networking campaigns in UI'S history!
+            
+        </p>
+            
+        </div>
+        </section>
